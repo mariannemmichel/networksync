1fbe0e6af8ed0390d1007f1e0f44cd5d21b8f20d
m.marinha.m <m.marinha.m@8c9652b4-9de4-11de-bf4a-bd5be0e2b6d7>
2009-10-07 08:06:59 +0000

    changed for plotting of observables for different parameters (sigmaW, senGain, actGain)

--- actualThesis/mainProcedure.m
+++ actualThesis/mainProcedure.m
@@ -13,62 +13,96 @@ M = coupFac * M;
 % measure time vector
 tSize = size(tSpan,1);
 
-% create solution vectors
-T = zeros(numRuns,1);
-Y = cell(1,numRuns);
-numSigma = numel(sigmaW);
-numSgain = numel(senGain);%!!!
-C = cell(1,numSgain);%!!!
-
 % param{1}(1) = number of community nodes
 % param{1}(2) = dimension of external system
-% param{2} = adjacency matrix of the whole network
+% param{2} = adjacency matrix of the internal network (community)
 % param{3} = natural frequencies of community nodes
 % param{4} = function handle to external system ode
-% param{5}{1} = nodes in community with sensors
-% param{5}{2} = handle to sensor function
-% param{5}{3} = internal states of external system connected to actuator
-% param{5}{4} = handle to actuator function
-% param{6} = parameters for the external ode function
+% param{5}(1) = adjacency matrix: sensor connection
+% param{5}(2) = handle to sensor function
+% param{5}(3) = adjacency matrix: actuator connection
+% param{5}(4) = handle to actuator function
+% param{6} = parameters for the external system
+param = cell(6,1);
 param{1} = [N numExtStates];
 param{2} = M;
 param{3} = zeros(N,1);
 param{4} = extFun;
-%param{5} = {sensorAdj, sensorFunc, actuatorAdj, actuatorFunc};
+param{5} = cell(1,4);
 param{6} = extParams;
 
+% create data matrices
+T = zeros(numRuns,1);
+numSigma = numel(sigmaW);
+numSgain = numel(senGain);
+numAgain = numel(actGain);
+
+increm = [];
+dispIncrem = '';
+numIncrem = 0;
+if numSigma>1
+    dispIncrem = 'Sigma';
+    increm = sigmaW;
+    numIncrem = numSigma;
+elseif numSgain>1
+    dispIncrem = 'Sensor Gain';
+    increm = senGain;
+    numIncrem = numSgain;
+    meanW = meanW * ones(1,numIncrem);
+    sigmaW = sigmaW * ones(1,numIncrem);
+elseif numAgain>1
+    dispIncrem = 'Actuator Gain';
+    increm = actGain;
+    numIncrem = numAgain;
+    meanW = meanW * ones(1,numIncrem);
+    sigmaW = sigmaW * ones(1,numIncrem);
+end
+    
+C = cell(1,numIncrem);
+Y = cell(numIncrem,numRuns);
+
+sensorAdj = senGain(1) * sensorAdj;
+actuatorAdj = actGain(1) * externAdj;
+            
 opt = odeset('RelTol',1e-6);
 
-for ss=1:numSigma
-    for sg=1:numSgain
-        sg
-        sensorAdj = senGain(sg) * sensorAdj;
-        param{5} = {sensorAdj, sensorFunc, actuatorAdj, actuatorFunc};
-        for i=1:numRuns
-            % calculate natural frequencies
-            param{3} = calcW(meanW(ss),sigmaW(ss),N);
-            
-            % ode
-            [T,Y{i}] = ode113(@(t,y) sync(t,y,param),tSpan,IC(:,i),opt);
-        end
-
-        % calculate corellation
-        CORavg = zeros(N,N,tSize);
-        for i=1:numRuns
-            COR = zeros(N,N,tSize);
-            for t=1:tSize
-                r = repmat(Y{i}(t,1:N),N,1);
-                COR(:,:,t) = cos(r'-r);
-            end
-            CORavg = CORavg + COR;
-        end
-        C{sg} = CORavg / numRuns;
-
+for s=1:numIncrem
+    
+    display(['Running ODE for ' dispIncrem ' = ' num2str(increm(s)) '...'])
+    
+    if strcmp(dispIncrem,'Sensor Gain')
+        sensorAdj = senGain(s) * sensorAdj;
+    elseif strcmp(dispIncrem,'Actuator Gain')
+        actuatorAdj = actGain(s) * externAdj;
     end
-end
+       
+    param{5} = {sensorAdj, sensorFunc, actuatorAdj, actuatorFunc};
+    
+    for i=1:numRuns
+        
+        display([ num2str(i) '. Run...'])
+        
+        % calculate natural frequencies
+        param{3} = calcW(meanW(s),sigmaW(s),N);
+        param{3}(1) = meanW(2); % hub gets no noise 
 
-% plot results
-%syncPlotSgain(T,Y,N,C,thresh,threshTau,senGain,'rhoinf','tau','rho','trajec');
+        % ode
+        [T,Y{s,i}] = ode113(@(t,y) sync(t,y,param),tSpan,IC(:,i),opt);
+    end
+
+    % calculate corellation
+    CORavg = zeros(N,N,tSize);
+    for i=1:numRuns
+        COR = zeros(N,N,tSize);
+        for t=1:tSize
+            r = repmat(Y{s,i}(t,1:N),N,1);
+            COR(:,:,t) = cos(r'-r);
+        end
+        CORavg = CORavg + COR;
+    end
+    C{s} = CORavg / numRuns;
+
+end
 
 
 % save results
