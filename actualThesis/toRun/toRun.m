748408abe00dccd61df4db7f743a77ac209216a5
m.marinha.m <m.marinha.m@8c9652b4-9de4-11de-bf4a-bd5be0e2b6d7>
2009-10-15 14:46:26 +0000

    minor changes

--- actualThesis/toRun/toRun.m
+++ actualThesis/toRun/toRun.m
@@ -4,11 +4,11 @@ clear all
 % add folders containing helper functions to path
 addpath('../externalSystems/','../plothelpers/')
 
-% load current parameters
-ipHopfSensorGain
-
 % change to parent directory
 cd ..
+
+% load current parameters
+ipHopfSensorGain
 
 % run main procedure
 mainProcedureToRun
