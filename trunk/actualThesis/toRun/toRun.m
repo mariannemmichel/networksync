d9fb75c55f0b57dd7a8b93e03d9193fee91b983f
m.marinha.m <m.marinha.m@8c9652b4-9de4-11de-bf4a-bd5be0e2b6d7>
2009-10-15 14:36:25 +0000

    thursdays changes plus settings for runs at the lab

---
+++ trunk/actualThesis/toRun/toRun.m
@@ -0,0 +1,14 @@
+
+clear all
+
+% load current parameters
+internalParams
+
+% change to parent directory
+cd ..
+
+% add folders containing helper functions to path
+addpath('./externalSystems/','./plothelpers/')
+
+% run main procedure
+mainProcedureToRun
