f87acdfed232cf7393534d47629adbe2dd72bc68
m.marinha.m <m.marinha.m@8c9652b4-9de4-11de-bf4a-bd5be0e2b6d7>
2009-10-15 14:38:36 +0000

    minor changes

--- actualThesis/plothelpers/spFreq.m
+++ actualThesis/plothelpers/spFreq.m
@@ -1,11 +1,14 @@
     
-function spFreq(ssFreqs,tellS,incrS)
+function spFreq(ssFreqs,tellS,incrS,extFreq)
     
     figure('Name','Steady State Frequencies','NumberTitle','off')
     labels = cellstr(num2str(incrS'));
     hold on
     boxplot(ssFreqs','labels',labels)
-    plot(incrS,1/pi)
+    plot(2*ones(size(incrS)),'bx')
+    plot(extFreq,'go')
+    a=axis();
+    axis([a(1:2) 0 4])
     hold off
     title('Steady State Frequencies')
     xlabel(tellS)
