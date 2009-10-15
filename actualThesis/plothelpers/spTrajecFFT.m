1477667fa67a52c8fcae8fe69283e378f42c82a0
m.marinha.m <m.marinha.m@8c9652b4-9de4-11de-bf4a-bd5be0e2b6d7>
2009-10-15 14:38:45 +0000

    minor changes

--- actualThesis/plothelpers/spTrajecFFT.m
+++ actualThesis/plothelpers/spTrajecFFT.m
@@ -1,6 +1,9 @@
 
-function spTrajecFFT(T,Y,N,incrS,tellS,doFFT,varargin)
+function spTrajecFFT(T,Y,N,incrS,tellS,Yindext,doFFT,varargin)
     
+    if isnan(Yindext)
+        error('Yindext not specified.')
+    end
     numS = size(Y,1);
     if doFFT
         if size(varargin,2) ~= 4
@@ -14,7 +17,8 @@ function spTrajecFFT(T,Y,N,incrS,tellS,doFFT,varargin)
         for s=1:numS
             clf
             subplot(1,2,1)
-            plot(T,sin(Y{s,1}(:,1)),'r-',T,sin(Y{s,1}(:,2:N)),'b-',T,sin(Y{s,1}(:,1)),'r-')
+            plot(T,sin(Y{s,1}(:,1)),'r-',T,sin(Y{s,1}(:,Yindext)),'g-',...
+                T,sin(Y{s,1}(:,2:N)),'b-',T,sin(Y{s,1}(:,1)),'r-')
             title(['Sample Community Trajectory for ' tellS '  = ' num2str(incrS(s))])
             legend('Hub','Nodes')
             xlabel('Time t')
@@ -34,9 +38,10 @@ function spTrajecFFT(T,Y,N,incrS,tellS,doFFT,varargin)
         figure('Name','Sample Trajectory of the Community','NumberTitle','off')
         for s=1:numS
             clf
-            plot(T,sin(Y{s,1}(:,1)),'r-',T,sin(Y{s,1}(:,2:N)),'b-',T,sin(Y{s,1}(:,1)),'r-')
+            plot(T,sin(Y{s,1}(:,1)),'r-',T,sin(Y{s,1}(:,Yindext)),'g-',...
+                T,sin(Y{s,1}(:,2:N)),'b-',T,sin(Y{s,1}(:,1)),'r-')
             title(['Sample Community Trajectory for ' tellS '  = ' num2str(incrS(s))])
-            legend('Hub','Nodes')
+            legend('Hub','External System','Nodes')
             xlabel('Time t')
             ylabel('sin( \theta )')
             set(gca,'YLim',[-1 1])
