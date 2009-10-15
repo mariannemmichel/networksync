a431ec09ea4dd38fb65f7fe21e7e8c493f6d74c7
m.marinha.m <m.marinha.m@8c9652b4-9de4-11de-bf4a-bd5be0e2b6d7>
2009-10-15 14:38:28 +0000

    minor changes

--- actualThesis/plothelpers/spCalcFFT.m
+++ actualThesis/plothelpers/spCalcFFT.m
@@ -1,13 +1,15 @@
 
-function [X,f,m,mind,ssFreqs] = spCalcFFT(Y,Fs,tauC)
+function [X,f,m,mind,ssFreqs,extFreq] = spCalcFFT(Y,Fs,tauC,Yindext)
 
     numS = size(Y,1);
     numRuns = size(Y,2);
     X = cell(1,numS);
+    extX = cell(1,numS);
     f = cell(1,numS);
     m = zeros(numS);
     mind = zeros(numS);
     ssFreqs = zeros(numS,numRuns);
+    extFreq = zeros(numS,numRuns);
     
     for s=1:numS
         x = Y{s,1}(tauC(s):end,1);
@@ -17,19 +19,32 @@ function [X,f,m,mind,ssFreqs] = spCalcFFT(Y,Fs,tauC)
         X{s} = 2*abs(X{s}(1:NFFT/2+1));
         [m(s),mind(s)] = max(X{s});
         if numel(m(s)) > 1
-            warning(1,'Warning: More than one dominant frequency at steady state.')
+            warning('Warning: More than one dominant frequency at steady state.')
         end
         f{s} = Fs/2*linspace(0,1,NFFT/2+1);
         ssFreqs(s,1) = f{s}(mind(s));
-        for i=1:numRuns-1
+        for i=2:numRuns
             x = Y{s,i}(tauC(s):end,1);
             XX = fft(sin(x),NFFT)/L;
             XX = 2*abs(XX(1:NFFT/2+1));
             [M,MI] = max(XX);
             if numel(M) > 1
-                warning(1,'Warning: More than one dominant frequency at steady state.')
+                warning('Warning: More than one dominant frequency at steady state.')
             end
             ssFreqs(s,i) = f{s}(MI);
         end
+    end
+    if ~isnan(Yindext)
+        for s=1:numS
+            for i=1:numRuns
+                extx = Y{s,i}(:,Yindext);
+                extX{s} = fft(sin(extx),NFFT)/L;
+                extX{s} = 2*abs(extX{s}(1:NFFT/2+1));
+                [plopm,plopind] = max(extX{s});
+                extFreq(s,i) = f{s}(plopind);
+            end 
+        end
+    else
+        warning('Warning: Index of external signals missing.')
     end
 end
